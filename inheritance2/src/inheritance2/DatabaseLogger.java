a309ae4ca6a342ad147139364a7975a9025c4c34
Casper <Casper@DESKTOP-IBKP7LK>
2021-05-13 22:29:16 +0300

    inheritance2

---
+++ inheritance2/src/inheritance2/DatabaseLogger.java
@@ -0,0 +1,11 @@
+package inheritance2;
+
+public class DatabaseLogger extends Logger
+{
+	   @Override
+	   public void Log()
+	   {
+		   System.out.println("Database loglandý");
+	   }
+
+}
---
+++ inheritance2/src/inheritance2/FileLogger.java
@@ -0,0 +1,11 @@
+package inheritance2;
+
+public class FileLogger extends Logger
+{
+	   @Override
+	   public void Log()
+	   {
+		   System.out.println("Dosya loglandý");
+	   }
+	   
+}
