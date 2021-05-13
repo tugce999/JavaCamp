0fb0de5295a8252e877ceaecb26038350e4b85ad
Casper <Casper@DESKTOP-IBKP7LK>
2021-05-14 01:05:08 +0300

    AbstractClasses

---
+++ AbstractClasses/src/GameCalculator.java
@@ -0,0 +1,11 @@
+
+public abstract class GameCalculator
+{
+   public abstract  void hesapla(); 
+  
+   
+   public void gameOver()
+   {
+	   System.out.println("Oyun bitti.");
+   }
+}
---
+++ AbstractClasses/src/ManGameCalculater.java
@@ -0,0 +1,9 @@
+
+public class ManGameCalculater  extends GameCalculator
+{
+	@Override
+	public void hesapla() 
+	   {
+		  System.out.println("Puanýnýz: 90");   
+	   }
+}
